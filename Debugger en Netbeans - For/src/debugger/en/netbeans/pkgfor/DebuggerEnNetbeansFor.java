375666b08f401819283923bf973654e6ef94a269
Lucas Javier Caron <lukascaron@gmail.com>
2019-05-11 23:30:43 -0300

    Actualizacion.

--- Debugger en Netbeans - For/src/debugger/en/netbeans/pkgfor/DebuggerEnNetbeansFor.java
+++ Debugger en Netbeans - For/src/debugger/en/netbeans/pkgfor/DebuggerEnNetbeansFor.java
@@ -15,7 +15,55 @@ public class DebuggerEnNetbeansFor {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /*
+        Declaración de las variables
+        */
+        
+        int numero = 0;
+        String [] nombre = {"Lucas", "Martín", "Laura"};
+        
+        /*
+        Utilización del bucle "while" para probar el debugger de Netbeans.
+        */
+        
+        while (numero < 10){ // Compara el contenido de la variable "numero", si es menor a 10, entra en el bucle.
+            
+            System.out.println(numero); // Se muestra el contenido de la variable "numero".
+            
+            numero++; // Aumenta, en 1, el contenido de la variable numero.
+        }
+        
+        
+        /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+        * El bucle "for" es un control de flujo que realiza, una porción de  *
+        * código, una determinada cantidad de veces.                         *
+         * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+        
+        /*
+        Utilización del bucle "for" para mostrar como aumenta un índice.
+        */
+        for (int indice = 0; indice < 10; indice++){ 
+            
+            System.out.println(indice);
+            
+        }
+        
+        /*
+        Utilización del bucle "for" para mostrar como disminuye un índice.
+        */
+        
+        for (int indice = 10; indice > 10; indice--){
+            
+            System.out.println(indice);
+            
+        }
+        
+        for (String i:nombre){
+            System.out.println(i);
+        }
+        
+        
+        
     }
     
 }

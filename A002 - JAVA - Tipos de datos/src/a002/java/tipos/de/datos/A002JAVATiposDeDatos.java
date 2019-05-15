f20ca9fafba08e3be2759f7dde8ec338101f9dba
Lucas Javier Caron <lukascaron@gmail.com>
2019-05-14 21:38:21 -0300

    Resolución del ejercicio A002 - JAVA - Tipos de datos

---
+++ A002 - JAVA - Tipos de datos/src/a002/java/tipos/de/datos/A002JAVATiposDeDatos.java
@@ -0,0 +1,29 @@
+/**
+ * Programa: A002 - JAVA - Tipos de datos
+ * Descripción: Revisar el comportamiento de los tipos de datos involucrados.
+ */
+package a002.java.tipos.de.datos;
+
+/**
+ *
+ * @author Caron, Lucas Javier
+ */
+public class A002JAVATiposDeDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        Declaración de las variables que vamos a utilizar.
+        */
+        
+        /*
+        Inicialización, de ser necesario, de las variables.
+        */
+        
+        // Imprimir el valor de una operación matemática.
+        System.out.println(2.0 * ( 5 / 2));
+    }
+    
+}

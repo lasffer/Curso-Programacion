72df307c0c64cd295fc3859a5568c33daa557621
Lucas Javier Caron <lukascaron@gmail.com>
2019-05-19 21:46:48 -0300

    Actualización de Debugger en Netbeans - For

--- Debugger en Netbeans - For/src/debugger/en/netbeans/pkgfor/DebuggerEnNetbeansFor.java
+++ Debugger en Netbeans - For/src/debugger/en/netbeans/pkgfor/DebuggerEnNetbeansFor.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
  */
 package debugger.en.netbeans.pkgfor;
 
 /**
  *
- * @author lasfferpro
+ * @author Caron, Lucas Javier
  */
 public class DebuggerEnNetbeansFor {
 
